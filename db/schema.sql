5800a37253fed10dde02caa67b6c3a634f971563
Hailey Burgess <hailey.burgess@redcross.org>
2025-07-28 19:34:49 -0400

    created db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,25 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS contacts;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
   username text NOT NULL UNIQUE,
   password text NOT NULL
+);
+
+CREATE TABLE events (
+  id serial PRIMARY KEY,
+  date date NOT NULL,
+  location text NOT NULL,
+  description text
+);
+
+CREATE TABLE contacts(
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  email varchar(225) NOT NULL, 
+  phone integer NOT NULL,
+  message text NOT NULL,
+  created_date date NOT NULL
 );
