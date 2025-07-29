92ea0715026798462750aa48b8bfdfd40be4379f
Hailey Burgess <hailey.burgess@redcross.org>
2025-07-28 20:51:58 -0400

    events GET route working

--- api/events.js
+++ api/events.js
@@ -1,18 +1,19 @@
 import express from "express";
-const router = express.Router;
+const router = express.Router();
 export default router;
 
 import { createEvent, getEvents } from "#db/queries/events";
 import requireBody from "#middleware/requireBody";
+import db from "#db/client";
 
 // Route to GET all events for /events page
 
-router.route("/events").get(async (req, res) => {
+router.route("/").get(async (req, res) => {
   try {
-    const departments = await getEvents();
+    const events = await getEvents();
     res.json(events);
   } catch (error) {
-    console.error("Error getting departments:", error);
+    console.error("Error getting events:", error);
     res.status(500).send("Internal server error");
   }
 });
