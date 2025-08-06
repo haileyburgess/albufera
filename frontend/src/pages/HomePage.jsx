80931c40f2d89c992e087bc32c614505b7c4163e
Hailey Burgess <hailey.burgess@redcross.org>
2025-08-06 11:33:36 -0400

    working on home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Events from "../components/Events";
+import HeroImage from "../components/HeroImage";
 
 export default function HomePage() {
   return (
-    <div>
-      <p className="about">
-        Welcome! We are a Brooklyn-based duo
-        serving authentic Valencian paella. We are committed to fresh
-        ingredients and sourcing all of our bomba rice, saffron, and paprika
-        directly from Valencia. <br></br>Albufera is the name of a lagoon 10km from
-        Valencia where paella was invented -- Local farmers used the ingredients
-        on hand to create a beloved dish of chicken, rabbit, bajoqueta, and
-        garrafo.
-      </p>
+    <div className="home">
       <div>
+        <p className="about">
+          <span>Welcome! We are a Brooklyn-based duo serving authentic Valencian
+          paella. We are committed to fresh ingredients and sourcing all of our
+          bomba rice, saffron, and paprika directly from Valencia.</span>
+          <span>Albufera is the name of a lagoon 10km from Valencia where paella was
+          invented -- Local farmers used the ingredients on hand to create a
+          beloved dish of chicken, rabbit, bajoqueta, and garrafo.</span>
+          <span>Serving traditional recipies and </span>
+        </p>
+      </div>
+      <div>
+        <HeroImage />
+      </div>
+      <div className="homeEvents">
         <Events />
       </div>
     </div>
