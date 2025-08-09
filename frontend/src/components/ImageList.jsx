7fd100600bad9995fdde3934b335c44c8aa5685f
Hailey Burgess <hailey.burgess@redcross.org>
2025-08-09 09:41:29 -0400

    resizing issue w/ home page elements

--- frontend/src/components/ImageList.jsx
+++ frontend/src/components/ImageList.jsx
@@ -6,14 +6,19 @@ import ImageListItem from "@mui/material/ImageListItem";
 
 export default function HomepageImageList() {
   return (
-    <ImageList sx={{ width: 600, height: 550 }} cols={3} rowHeight={190}>
+    <ImageList
+      sx={{ width: "100%", height: "auto" }}
+      cols={3}
+      rowHeight={"auto"}
+    >
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
           <img
             srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
             src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-            alt={item.title} 
+            alt={item.title}
             loading="lazy"
+            style={{ width: "100%", height: "auto" }}
           />
         </ImageListItem>
       ))}
