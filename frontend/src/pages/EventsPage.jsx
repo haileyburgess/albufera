1cccce24cd5fe2c59b4c524343e03ab9ff9f89ea
Hailey Burgess <hailey.burgess@redcross.org>
2025-08-13 19:13:08 -0400

    react social media embed library

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Events, { FutureEvents } from "../components/Events";
 import { EventForm } from "../components/CreateEvent";
 import { UpdateEvent } from "../components/UpdateEvent";
+import { InstagramEmbed } from "react-social-media-embed";
 export default function EventsPage() {
   return (
     <div className="events">
@@ -13,6 +14,15 @@ export default function EventsPage() {
       </div>
       <div className="event-form">
         <EventForm />
+      </div>
+      <div
+        className="insta"
+        style={{ display: "grid", justifyContent: "center" }}
+      >
+        <InstagramEmbed
+          url="https://www.instagram.com/p/DLk-3CWSFPD/"
+          width="auto"
+        />
       </div>
     </div>
   );
